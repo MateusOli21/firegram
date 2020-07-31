a750f8317c745941ede683cc16c978919966ee57
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-07-31 19:08:40 -0300

    Changed useStorage to store image url on firebase database.

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { projectStorage } from "../firebase/config";
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from "../firebase/config";
 
 function useStorage(file) {
   const [progress, setProgress] = useState(0);
@@ -8,6 +12,7 @@ function useStorage(file) {
 
   useEffect(() => {
     const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection("images");
 
     storageRef.put(file).on(
       "state_changed",
@@ -20,10 +25,14 @@ function useStorage(file) {
       },
       async () => {
         const imageUrl = await storageRef.getDownloadURL();
+
+        const createdAt = timestamp();
+        collectionRef.add({ url, createdAt });
+
         setUrl(imageUrl);
       }
     );
-  }, [file]);
+  }, [file, url]);
 
   return { progress, url, error };
 }
